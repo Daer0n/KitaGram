6ced37e718a746c4b6365bcabcb13ee183377b74
Daer0n <danilatarbaya57@gmail.com>
2024-11-30 15:33:34 +0300

    feat: add options form

--- frontend/src/components/OptionsForm/index.tsx
+++ frontend/src/components/OptionsForm/index.tsx
@@ -1,18 +1,68 @@
-import { Avatar } from "antd";
-import { Container, Content, Info, Title } from "./styled";
+import Arrow from '@assets/images/Arrow.svg';
+import DefaultAvatar from '@assets/images/DefaultAvatar.svg';
+import {
+    Container,
+    Content,
+    Description,
+    Info,
+    Section,
+    SectionElement,
+    Avatar,
+    Button,
+} from './styled';
+import { useNavigate } from 'react-router-dom';
 
-export const OptionsForm =() => {
-    return <Container>
-            <Content>   
+export const OptionsForm = () => {
+    const navigate = useNavigate();
+    return (
+        <Container>
+            <Content>
                 <Info>
-                    <Title>
-                        Настройки
-                    </Title>
-
                     <Avatar>
-                    
+                        <img src={DefaultAvatar} alt="default avatar" />
                     </Avatar>
                 </Info>
+
+                <Section>
+                    <Description>Настройки</Description>
+
+                    <SectionElement>
+                        Аккаунт
+                        <img src={Arrow} alt="" />
+                    </SectionElement>
+
+                    <SectionElement>
+                        Уведомления
+                        <img src={Arrow} alt="" />
+                    </SectionElement>
+                </Section>
+
+                <Section>
+                    <Description>Информация</Description>
+
+                    <SectionElement>
+                        Конфиденциальность
+                        <img src={Arrow} alt="" />
+                    </SectionElement>
+
+                    <SectionElement>
+                        Обратная связь
+                        <img src={Arrow} alt="" />
+                    </SectionElement>
+
+                    <SectionElement>
+                        Оценить KitaGram
+                        <img src={Arrow} alt="" />
+                    </SectionElement>
+
+                    <SectionElement>
+                        Поделиться приложением
+                        <img src={Arrow} alt="" />
+                    </SectionElement>
+                </Section>
+
+                <Button onClick={() => navigate('/')}>Выйти из аккаунта</Button>
             </Content>
-    </Container>
-}
+        </Container>
+    );
+};
