f702c2c0649482aada500615cd3f627db360df89
Alexandr Rutkowski <kitanoyoru@protonmail.com>
2024-12-17 01:26:10 +0300

    feat: add yandex map

--- frontend/src/components/Tags/index.tsx
+++ frontend/src/components/Tags/index.tsx
@@ -2,33 +2,33 @@ import { Tag } from '@types';
 import { Container, Element } from './styled';
 
 interface TagsProps {
-    tags: Tag[];
-    selectedTags: number[];
-    setSelectedTags: (tagsId: number[]) => void;
+  tags: Tag[];
+  selectedTags: number[];
+  setSelectedTags: (tagsId: number[]) => void;
 }
 
 export const Tags: React.FC<TagsProps> = ({ tags, selectedTags, setSelectedTags }) => {
-    const toggleTag = (tagID: number) => {
-        setSelectedTags((prevSelectedTags: number[]) => {
-            if (prevSelectedTags.includes(tagID)) {
-                return prevSelectedTags.filter((id: number) => id !== tagID);
-            } else {
-                return [...prevSelectedTags, tagID];
-            }
-        });
-    };
+  const toggleTag = (tagID: number) => {
+    setSelectedTags((prevSelectedTags: number[]) => {
+      if (prevSelectedTags.includes(tagID)) {
+        return prevSelectedTags.filter((id: number) => id !== tagID);
+      } else {
+        return [...prevSelectedTags, tagID];
+      }
+    });
+  };
 
-    return (
-        <Container>
-            {tags.map((tag: Tag) => (
-                <Element
-                    key={tag.id}
-                    onClick={() => toggleTag(tag.id)}
-                    isSelected={selectedTags.includes(tag.id)}
-                >
-                    {tag.name}
-                </Element>
-            ))}
-        </Container>
-    );
+  return (
+    <Container>
+      {tags.map((tag: Tag) => (
+        <Element
+          key={tag.id}
+          onClick={() => toggleTag(tag.id)}
+          isSelected={selectedTags.includes(tag.id)}
+        >
+          {tag.name}
+        </Element>
+      ))}
+    </Container>
+  );
 };
